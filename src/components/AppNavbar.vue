7f8a5dc57c2103e0e1f0048a7460fc1583d8a10b
blurryfacebeat <blurryfacebeat@gmail.com>
2021-02-23 23:26:00 +0700

    add layouts

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar navbar-light">
+    <div class="container">
+      <a class="navbar-brand" href="index.html">conduit</a>
+      <ul class="nav navbar-nav pull-xs-right">
+        <li class="nav-item">
+          <!-- Add "active" class when you're on that page" -->
+          <router-link class="nav-link active" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">
+            <i class="ion-compose"></i>&nbsp;New Post
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/settings">
+            <i class="ion-gear-a"></i>&nbsp;Settings
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/register">Sign up</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    
+  })
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,34 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/',
+    name: 'home',
+    component: () =>  import('@/views/Home.vue')
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    path: '/login',
+    name: 'login',
+    component: () => import('@/components/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('@/components/Register.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/components/Login.vue')
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: () => import('@/components/Settings.vue')
+  },
 ];
 
 const router = new VueRouter({
